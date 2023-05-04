a91256b6717e0a65318cd24981b3c3b7e4e8bdf9
Johnny Martinez <mrtnz.jn@gmail.com>
2023-05-04 14:33:02 -0500

    completed maven exercises

--- src/MavenExercises.java
+++ src/MavenExercises.java
@@ -1,6 +1,34 @@
+import java.util.Scanner;
+import org.apache.commons.lang3.StringUtils;
+
 public class MavenExercises {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+//        Checks for a number
+//        System.out.println("please enter a number:");
+//        String userInput = sc.nextLine();
+//
+//        if (StringUtils.isNumeric(userInput)){
+//            System.out.println("true");
+//        }else{
+//            System.out.println("Not a Number.");
+//        }
+
+//        REVERSE CASE
+
+//        System.out.println("Enter a word:");
+//        String userWord = sc.nextLine();
+//
+//        String reverseCase = StringUtils.swapCase(userWord);
+//        System.out.println(reverseCase);
+
+        System.out.println("Enter a word:");
+        String reverseWord = sc.nextLine();
+
+        String reverse = StringUtils.reverse(reverseWord);
+        System.out.println(reverse);
     }
 }
